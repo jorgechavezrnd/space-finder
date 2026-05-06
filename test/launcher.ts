b7ed94ec8a6db519a0ed0d2c00eba86e56c52860
jorgechavezrnd <jorgechavezrnd@gmail.com>
2026-05-05 21:58:33 -0400

    Validation

--- test/launcher.ts
+++ test/launcher.ts
@@ -4,11 +4,13 @@ process.env.AWS_REGION = 'us-east-1';
 process.env.TABLE_NAME = 'SpaceTable-121b4872df75';
 
 handler({
-  httpMethod: 'DELETE',
-  queryStringParameters: {
-    id: '1b89b945-0805-4fdc-bcdb-87ea127be1e6'
-  },
-  // body: JSON.stringify({
-  //   location: 'Dublin updated'
-  // })
-} as any, {} as any);
+  httpMethod: 'POST',
+  // queryStringParameters: {
+  //   id: '1b89b945-0805-4fdc-bcdb-87ea127be1e6'
+  // },
+  body: JSON.stringify({
+    location: 'Dublin updated'
+  })
+} as any, {} as any).then(result => {
+  console.log(result);
+});
