2c4262e2594a31bfacc52fabf8de59cb9fb3d227
jorgechavezrnd <jorgechavezrnd@gmail.com>
2026-05-05 20:22:44 -0400

    Get Item

--- test/launcher.ts
+++ test/launcher.ts
@@ -5,6 +5,9 @@ process.env.TABLE_NAME = 'SpaceTable-121b4872df75';
 
 handler({
   httpMethod: 'GET',
+  queryStringParameters: {
+    id: '1b89b945-0805-4fdc-bcdb-87ea127be1e6'
+  }
   // body: JSON.stringify({
   //   location: 'Dublin'
   // })
