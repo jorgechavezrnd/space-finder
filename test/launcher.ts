d4e384c8e1457d3c97caf87404393fe46a1ed6b7
jorgechavezrnd <jorgechavezrnd@gmail.com>
2026-05-05 21:40:44 -0400

    Delete Item

--- test/launcher.ts
+++ test/launcher.ts
@@ -4,11 +4,11 @@ process.env.AWS_REGION = 'us-east-1';
 process.env.TABLE_NAME = 'SpaceTable-121b4872df75';
 
 handler({
-  httpMethod: 'PUT',
+  httpMethod: 'DELETE',
   queryStringParameters: {
     id: '1b89b945-0805-4fdc-bcdb-87ea127be1e6'
   },
-  body: JSON.stringify({
-    location: 'Dublin updated'
-  })
+  // body: JSON.stringify({
+  //   location: 'Dublin updated'
+  // })
 } as any, {} as any);
