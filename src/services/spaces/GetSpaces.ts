43312a8d0469a4e66d1a4d37f4d5485af19bf58f
jorgechavezrnd <jorgechavezrnd@gmail.com>
2026-05-05 20:33:56 -0400

    Marshalling - @aws-sdk/util-dynamodb

--- src/services/spaces/GetSpaces.ts
+++ src/services/spaces/GetSpaces.ts
@@ -1,4 +1,5 @@
 import { DynamoDBClient, GetItemCommand, ScanCommand } from '@aws-sdk/client-dynamodb';
+import { unmarshall } from '@aws-sdk/util-dynamodb';
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 
 export async function getSpaces(event: APIGatewayProxyEvent, ddbClient: DynamoDBClient): Promise<APIGatewayProxyResult> {
@@ -12,9 +13,10 @@ export async function getSpaces(event: APIGatewayProxyEvent, ddbClient: DynamoDB
         }
       }));
       if (getItemResponse.Item) {
+        const unmarshalledItem = unmarshall(getItemResponse.Item);
         return {
           statusCode: 200,
-          body: JSON.stringify(getItemResponse.Item)
+          body: JSON.stringify(unmarshalledItem)
         };
       } else {
         return {
@@ -34,10 +36,11 @@ export async function getSpaces(event: APIGatewayProxyEvent, ddbClient: DynamoDB
     TableName: process.env.TABLE_NAME
   }));
 
-  console.log(result.Items);
+  const unmarshalledItems = result.Items?.map(item => unmarshall(item));
+  console.log(unmarshalledItems);
 
   return {
     statusCode: 200,
-    body: JSON.stringify(result.Items)
+    body: JSON.stringify(unmarshalledItems)
   };
 }
