c8452e3554b77685a10f1043ea02aaa69b979e80
jorgechavezrnd <jorgechavezrnd@gmail.com>
2026-05-17 10:49:47 -0400

    Update webhook URL

--- src/services/monitor/handler.ts
+++ src/services/monitor/handler.ts
@@ -1,6 +1,6 @@
 import { SNSEvent } from 'aws-lambda';
 
-const webHookUrl = 'https://hooks.slack.com/services/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';
+const webHookUrl = 'https://hooks.slack.com/services/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';
 
 async function handler(event: SNSEvent, context: any) {
   for (const record of event.Records) {
