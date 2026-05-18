16847a757cdeaf9992f4025cf1af031a517ffd18
jorgechavezrnd <jorgechavezrnd@gmail.com>
2026-05-17 20:49:23 -0400

    CDK declarative tests

---
+++ test/infra/MonitorStack.test.ts
@@ -0,0 +1,31 @@
+import { App } from 'aws-cdk-lib';
+import { MonitorStack } from '../../src/infra/stacks/MonitorStack';
+import { Template } from 'aws-cdk-lib/assertions';
+
+describe('Initial test suite', () => {
+
+  let monitorStackTemplate: Template;
+
+  beforeAll(() => {
+    const testApp = new App({
+      outdir: 'cdk.out'
+    });
+    const monitorStack = new MonitorStack(testApp, 'MonitorStack');
+    monitorStackTemplate = Template.fromStack(monitorStack);
+  });
+
+  test('Lambda properties', () => {
+    monitorStackTemplate.hasResourceProperties('AWS::Lambda::Function', {
+      Handler: 'index.handler',
+      Runtime: 'nodejs24.x'
+    });
+  });
+
+  test('Sns topic properties', () => {
+    monitorStackTemplate.hasResourceProperties('AWS::SNS::Topic', {
+      DisplayName: 'AlarmTopic',
+      TopicName: 'AlarmTopic'
+    });
+  });
+
+});
